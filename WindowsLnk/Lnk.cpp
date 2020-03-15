55da109ee1e285831cd4d17556e6ab68b1c5c1a2
dex black <dex@dexblack.net>
2020-03-15 17:09:02 +1100

    Added several basic validation requirements.

--- WindowsLnk/Lnk.cpp
+++ WindowsLnk/Lnk.cpp
@@ -8,6 +8,10 @@
 #include <vector>
 #include "Lnk.hpp"
 
+// 00021401-0000-0000-C000-000000000046
+const CLSID LnkHeader::cLnkCLSID
+{ 0x00021401, 0x0000, 0x0000, { 0x00, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46} };
+
 
 // Base class LnkHeader takes care of basic initialisation.
 //
@@ -17,10 +21,39 @@ Lnk::Lnk()
 
 // Fixed size required.
 //
-bool Lnk::isValidHeader() const
+bool Lnk::isValidHeaderSize() const
 {
   return LnkHeader::cRequiredSize == sizeof(header);
 }
+
+// Must a fixed CLSID.
+bool Lnk::isValidCLSID() const
+{
+  return LnkHeader::cLnkCLSID == header.clsid;
+}
+
+
+bool Lnk::isValidReserved() const
+{
+  return
+    header.Reserved1 == 0u &&
+    header.Reserved2 == 0U &&
+    header.Reserved3 == 0U;
+}
+
+
+bool Lnk::isValidShowCommand() const
+{
+  switch (header.show_command)
+  {
+  case SW_SHOWNORMAL:
+  case SW_SHOWMAXIMIZED:
+  case SW_SHOWMINNOACTIVE:
+    return true;
+  }
+  return false;
+}
+
 
 // Validation conditions for checking object state.
 //
@@ -31,11 +64,15 @@ bool Lnk::isValid() const
     bool valid; char const* error;
   };
 
-  std::vector<validity_condition> validity
+  std::vector<validity_condition> validity_conditions
   {
-    {isValidHeader(), "Invalid HeaderSize"}
+    { isValidHeaderSize(), "Invalid HeaderSize" },
+    { isValidCLSID(), "Invalid Lnk CLSID" },
+    { isValidShowCommand(), "Invalid Show Command value" },
+    { isValidReserved(), "Invalid Reserved bytes" },
   };
-  for (validity_condition const& vc : validity)
+
+  for (validity_condition const& vc : validity_conditions)
   {
     if (!vc.valid)
     {
--- WindowsLnk/Lnk.hpp
+++ WindowsLnk/Lnk.hpp
@@ -16,8 +16,22 @@ struct LnkDllPort LnkHeader
 {
   // Required header size.
   static const uint16_t cRequiredSize = 0x4C;
+  static const CLSID cLnkCLSID;
 
-  char buf[cRequiredSize];
+  uint32_t size = cRequiredSize;
+  CLSID    clsid = cLnkCLSID;
+  uint32_t link_flags;
+  uint32_t file_attrib;
+  FILETIME creation_time;
+  FILETIME access_time;
+  FILETIME write_time;
+  uint32_t file_size;
+  uint32_t icon_index = 0;
+  uint32_t show_command = SW_SHOWNORMAL;
+  uint16_t hot_key = 0;
+  uint16_t Reserved1 = 0;
+  uint32_t Reserved2 = 0;
+  uint32_t Reserved3 = 0;
 };
 
 
@@ -36,5 +50,8 @@ public:
   bool isValid() const;
 
 private:
-  bool isValidHeader() const;
+  bool isValidHeaderSize() const;
+  bool isValidCLSID() const;
+  bool isValidShowCommand() const;
+  bool isValidReserved() const;
 };
