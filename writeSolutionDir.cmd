a5cee12e8f47363433d56f37202b4d3b76e49f20
dex black <dex@dexblack.net>
2020-03-15 20:38:38 +1100

    Corrected output append bugs and hid more output

--- writeSolutionDir.cmd
+++ writeSolutionDir.cmd
@@ -1,25 +1,26 @@
 @echo off
 setlocal EnableExtensions 
 set output_dir=%~dp0
-set output_file=%output_dir%TestLnk\SolutionDir.cpp
-set output_file_new=%output_dir%TestLnk\SolutionDirNew.cpp
+set output_file_prefix=%output_dir%TestLnk\SolutionDir
+set output_file=%output_file_prefix%.cpp
+set output_file_new=%output_file_prefix%New.cpp
 set output_dir=%output_dir:\=\\%
 rem echo %output_dir%
 rem echo Writing $(SolutionDir) to %output_file_new%
 echo #include "pch.h" > %output_file_new%
-echo #include "SolutionDir.h" > %output_file_new%
-echo. > %output_file_new%
-echo const std::string TestData::SolutionDir { "%output_dir%"};
+echo #include "SolutionDir.h" >> %output_file_new%
+echo. >> %output_file_new%
+echo const std::string TestData::SolutionDir { "%output_dir%"}; >> %output_file_new%
 rem echo %output_file_new% created
 rem echo Checking for differences
 fc /L "%output_file%" "%output_file_new%" >NUL
 if ERRORLEVEL 1 goto :CHANGED
 rem echo Unchanged
-if exist %output_file_new% del %output_file_new%
+if exist %output_file_new% del %output_file_new% >NUL
 goto :END
 :CHANGED
 echo Replacing "%output_file%"
-del "%output_file%"
-move "%output_file_new%" "%output_file%"
+del "%output_file%" >NUL
+move "%output_file_new%" "%output_file%" >NUL
 :END
 endlocal
