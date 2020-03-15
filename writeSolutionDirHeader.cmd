bd4e1676ad8c28a8ef28fbbcfd67ba5d02b0d9a7
dex black <dex@dexblack.net>
2020-03-15 18:19:46 +1100

    Removed pause at end of script and improved log output

--- writeSolutionDirHeader.cmd
+++ writeSolutionDirHeader.cmd
@@ -1,17 +1,17 @@
 @echo off
 setlocal EnableExtensions 
-echo Writing $(SolutionDir) to
 set output_dir=%~dp0
 set output_file=%output_dir%TestLnk\SolutionDir.h
 set output_file_new=%output_dir%TestLnk\SolutionDirNew.h
 set output_dir=%output_dir:\=\\%
-echo %output_dir%
+rem echo %output_dir%
+rem echo Writing $(SolutionDir) to %output_file_new%
 echo struct TestData { char const* const SolutionDir { "%output_dir%"}; }; > %output_file_new%
 echo %output_file_new% created
-echo Checking for differences
+rem echo Checking for differences
 fc /L "%output_file%" "%output_file_new%" >NUL
 if ERRORLEVEL 1 goto :CHANGED
-echo Unchanged
+rem echo Unchanged
 if exist %output_file_new% del %output_file_new%
 goto :END
 :CHANGED
@@ -20,4 +20,3 @@ del "%output_file%"
 move "%output_file_new%" "%output_file%"
 :END
 endlocal
-pause
