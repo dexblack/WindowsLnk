bf33daa003abc12c54322385ec034bc85a25b217
dex black <dex@dexblack.net>
2020-03-19 20:46:13 +1100

    Added some type declarations for use in upcoming work.

--- WindowsLnk/base_types.h
+++ WindowsLnk/base_types.h
@@ -1,3 +1,6 @@
 #pragma once
 
 typedef unsigned char byte;
+
+typedef std::vector<byte> ItemID;    // SHITEMID. See PIDL.hpp
+typedef std::vector<ItemID> ItemIDs; // ItemIDList
--- WindowsLnk/framework.h
+++ WindowsLnk/framework.h
@@ -3,3 +3,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+#include <Knownfolders.h>
+#include <combaseapi.h>
+
+#if defined(_WIN32) && !defined(OLE2ANSI)
+# define TESTLNK_WIDE_OLECHAR
+#endif
