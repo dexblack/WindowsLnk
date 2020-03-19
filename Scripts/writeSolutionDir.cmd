5d91219373dc7b95a301d9222887725e57069f03
dex black <dex@dexblack.net>
2020-03-20 10:56:57 +1100

    Tested changes to writeSolutionDir.cmd in Scripts project

--- Scripts/writeSolutionDir.cmd
+++ Scripts/writeSolutionDir.cmd
@@ -1,17 +1,18 @@
 @echo off
 set status=0
 setlocal EnableExtensions 
-set output_dir=%~dp0
-set output_file_prefix=%output_dir%TestLnk\SolutionDir
+for %%i in ("%~dp0..") do set solution_dir=%%~fi\
+echo solution_dir %solution_dir%
+set output_file_prefix=%solution_dir%TestLnk\SolutionDir
 set output_file=%output_file_prefix%.cpp
 set output_file_new=%output_file_prefix%New.cpp
-set output_dir=%output_dir:\=\\%
-rem echo %output_dir%
+set solution_dir=%solution_dir:\=\\%
+rem echo %solution_dir%
 rem echo Writing $(SolutionDir) to %output_file_new%
 echo #include "pch.h" > %output_file_new%
 echo #include "SolutionDir.h" >> %output_file_new%
 echo. >> %output_file_new%
-echo const std::string TestData::SolutionDir { "%output_dir%"}; >> %output_file_new%
+echo const std::string TestData::SolutionDir { "%solution_dir%"}; >> %output_file_new%
 rem echo %output_file_new% created
 rem echo Checking for differences
 fc /L "%output_file%" "%output_file_new%" >NUL
@@ -25,5 +26,5 @@ rem echo Replacing "%output_file%"
 del "%output_file%" >NUL
 move "%output_file_new%" "%output_file%" >NUL
 :END
-exit /b %status%
+rem exit /b %status%
 endlocal
